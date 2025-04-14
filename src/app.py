f33fcd0c28bb9872618c450e46f77bfaed2c3f8b
Giovannytrotta <giovannytrotta13@gmail.com>
2025-04-14 14:54:32 +0000

    family

--- src/app.py
+++ src/app.py
@@ -30,14 +30,31 @@ def sitemap():
 
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
+def get_all_members():
+      members = jackson_family.get_all_members()
     # This is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {"hello": "world",
-                     "family": members}
-    return jsonify(response_body), 200
+      return jsonify(members), 200
 
+@app.route('/members', methods=['POST'])
+def handle_add_member():
+    body = request.get_json()
+    added_member=jackson_family.add_member(body)
+    
+    return jsonify(added_member), 200
 
+@app.route('/members/<int:member_id>', methods=['GET'])
+def find_member(member_id):
+    member = jackson_family.get_member(member_id)
+    if member is None:
+        return jsonify({"Msg": "Member not found"}),400
+    return jsonify(member),200
+
+@app.route('/members/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    deleted= jackson_family.delete_member(member_id)
+    if not deleted:
+        return jsonify({"done": False}),400
+    return jsonify({"done": True}), 200
 
 # This only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -10,13 +10,29 @@ class FamilyStructure:
         self.last_name = last_name
         self._next_id = 1
         self._members = [
-            {
-                "id": self._generate_id(),
+            
+                {  # Miembro 1: John Jackson
+                "id": self._generate_id(),  # Autogenera ID 
                 "first_name": "John",
-                "last_name": last_name,
+                "last_name": self.last_name,  # "Jackson"
                 "age": 33,
                 "lucky_numbers": [7, 13, 22]
+            },
+            {  # Miembro 2: Jane Jackson
+                "id": self._generate_id(),  # Autogenera ID 
+                "first_name": "Jane",
+                "last_name": self.last_name,  # "Jackson"
+                "age": 35,
+                "lucky_numbers": [10, 14, 3]
+            },
+            {  # Miembro 3: Jimmy Jackson
+                "id": self._generate_id(),  # Autogenera ID 
+                "first_name": "Jimmy",
+                "last_name": self.last_name,  # "Jackson"
+                "age": 5,
+                "lucky_numbers": [1]
             }
+            
         ]
 
     # This method generates a unique incremental ID
@@ -24,22 +40,43 @@ class FamilyStructure:
         generated_id = self._next_id
         self._next_id += 1
         return generated_id
+    
+    #PARA AGREGAR MIEMBROS 
 
     def add_member(self, member):
+        if "id" not in member: #GUARDAR UN ID SI NO EXISTE
+            member["id"]= self._generate_id() #ASIGNAR UN ID AUTOMATICAMENTE
+        member["last_name"]=self.last_name
+        print(member)
+        self._members.append(member)
+        return member
+            
         ## You have to implement this method
         ## Append the member to the list of _members
-        pass
+        
 
     def delete_member(self, id):
+        member_index=-1
+        for i,member in enumerate(self._members):
+            if member ["id"] ==id:
+                member_index=i
+                break
+        if member_index==-1:
+            return False
+        self._members.pop(member_index)
+        return True
         ## You have to implement this method
         ## Loop the list and delete the member with the given id
-        pass
+        
 
     def get_member(self, id):
+        for member in self._members:
+            if member ["id"] == id:
+                return member
+        return None
         ## You have to implement this method
         ## Loop all the members and return the one with the given id
-        pass
-
+        
     # This method is done, it returns a list with all the family members
     def get_all_members(self):
         return self._members
